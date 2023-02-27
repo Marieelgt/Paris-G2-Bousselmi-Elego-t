776143cbd9ab13e8daf8e1bbff0306601f12bad7
GaranceMnr <126480372+GaranceMnr@users.noreply.github.com>
2023-02-27 14:41:10 +0100

    Update Python test1 - Gyhub.py

--- Python test1 - Gyhub.py
+++ Python test1 - Gyhub.py
@@ -8,3 +8,4 @@ Created on Mon Feb 27 14:30:29 2023
 
 print("holà ! Soy Marie. Me gusta escuchar la musica")
 print("hola Marie ! Te deseo la Bienvenida !")
+print("hola chicas! paella, sangria y playa!")
